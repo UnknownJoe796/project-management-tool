57ee00e41dee94e332361b1499f19f123ea3a45a
Joseph Ivie <josephivie@gmail.com>
2017-06-19 18:08:18 -0600

    Xodus DB is working!

--- project-management-server/src/main/kotlin/Main.kt
+++ project-management-server/src/main/kotlin/Main.kt
@@ -2,11 +2,11 @@
  * Created by josep on 5/31/2017.
  */
 
-import com.ivieleague.kotlin.server.MemoryDatabaseAccess
 import com.ivieleague.kotlin.server.SecurityTableAccess
 import com.ivieleague.kotlin.server.core.Instance
-import com.ivieleague.kotlin.server.core.Write
 import com.ivieleague.kotlin.server.restPlus
+import com.ivieleague.kotlin.server.xodus.XodusAccess
+import jetbrains.exodus.entitystore.PersistentEntityStores
 import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.http.ContentType
 import org.jetbrains.ktor.netty.Netty
@@ -20,15 +20,17 @@ data class LoginInfo(var email: String = "", var password: String = "")
 fun main(vararg strings: String) {
 
 //    Database.connect("127.0.0.1:5432", "org.postgresql.Driver", user = "postgres", password = "4E&619>r0Kxl")
+    val xodusEntityStore = PersistentEntityStores.newInstance("C:\\XodusTest\\")
+    val xodus = XodusAccess(xodusEntityStore)
 
-    MemoryDatabaseAccess[Note].update(null, Write(
-            id = null,
-            scalars = mapOf(Note.title to "Example", Note.content to "Some example content"),
-            links = mapOf(Note.related to Write(
-                    id = null,
-                    scalars = mapOf(Note.title to "Related", Note.content to "Don't mind me.")
-            ))
-    ))
+//    xodus[Note].update(null, Write(
+//            id = null,
+//            scalars = mapOf(Note.title to "Example", Note.content to "Some example content"),
+//            links = mapOf(Note.related to Write(
+//                    id = null,
+//                    scalars = mapOf(Note.title to "Related", Note.content to "Don't mind me.")
+//            ))
+//    ))
 
     embeddedServer(Netty, 8080) {
         routing {
@@ -37,7 +39,7 @@ fun main(vararg strings: String) {
             }
             route("rest") {
                 route("note") {
-                    restPlus(SecurityTableAccess(MemoryDatabaseAccess[Note]), {
+                    restPlus(SecurityTableAccess(xodus[Note]), {
                         if (it.request.headers["Authorization"] != null)
                             Instance("283912", mapOf(), mapOf(), mapOf())
                         else null
